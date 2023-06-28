e07f38b81897b59fab3b8befe174cac55d76757e
Rafael Abreu <rafael.abreu031015@gmail.com>
2023-06-28 00:52:38 -0300

    update

--- connection/database.js
+++ connection/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2')
 const conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '2546978',
+  password: '',
   database: 'sistemakling'
 })
 
